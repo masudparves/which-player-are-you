490ea7705f91eabfcf4f5ec9fa8227a1423ea883
masudparves <parves.android@gmail.com>
2026-06-20 15:19:20 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Base "./" makes the build work on Netlify, Vercel, Cloudflare Pages, and sub-folders.
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      external: [/^firebase(\/.*)?$/],
+    },
+  },
 });
